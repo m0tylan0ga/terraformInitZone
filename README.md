df551fe8675c87202686c129aea309ac65262695
Wolnicki <mlwc@gft.com>
2019-04-26 08:58:50 +0200

    init terra script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # terraformInitZone
 terraformInitZone
+
+Example repo to run terraform script using arn role istead of aws credentials
---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  assume_role {
+    role_arn     = "${replace(data.local_file.foo.content,"\n","")}"
+  }
+  region     = "eu-west-2"
+}
+
+data "local_file" "foo" {
+    filename = "arn.txt"
+}
+
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  count = "5"
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "jjj"
+  }
+}
+
+
