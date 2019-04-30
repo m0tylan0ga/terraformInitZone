3d72b43656da3604a660aa8ef21911702db8f4bb
m0tylan0ga <wolnicki987@gmail.com>
2019-04-30 16:42:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,17 +5,6 @@ provider "aws" {
   region     = "${replace(data.local_file.region.content,"\n","")}"
 }
 
-terraform {
-  backend "s3" {
-    #bucket name here is hardcoded - we should try to create s3 bucket automatically by cloud formation and then try to find it eg by tag in terraform
-    bucket = "tbase-terra-lock-bucket-initial-landing-zone-aws"
-    region  = "eu-west-2"
-    dynamodb_table = "terraform-lock"
-    key = "terraform/dev/terraform_dev.tfstate"
-    encrypt = true
-  }
-}
-
 data "local_file" "arn" {
     filename = "/opt/arn.txt"
 }
