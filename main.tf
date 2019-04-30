3796077f2e075124abaad372afff6517df680c67
m0tylan0ga <wolnicki987@gmail.com>
2019-04-30 13:33:09 +0200

    rename bucket

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     #bucket name here is hardcoded - we should try to create s3 bucket automatically by cloud formation and then try to find it eg by tag in terraform
-    bucket = "tbase-terra-lock-bucket-initial-landing-zone"
+    bucket = "tbase-terra-lock-bucket-initial-landing-zone-aws"
     region  = "eu-west-2"
     dynamodb_table = "terraform-lock"
     key = "terraform/dev/terraform_dev.tfstate"
