35788f7df4e6842f5a107617cb65f3eee4928de3
m0tylan0ga <wolnicki987@gmail.com>
2019-05-01 15:03:36 +0200

    Update backend-tf-state.tf

--- backend-tf-state.tf
+++ backend-tf-state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     #bucket name here is hardcoded - we should try to create s3 bucket automatically by cloud formation and then try to find it eg by tag in terraform
-    bucket
+    bucket =
     region  = "eu-west-2"
     dynamodb_table = "terraform-lock"
     key = "terraform/dev/terraform_dev.tfstate"
